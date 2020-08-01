d8daaab1ece07db0b4eff9dacbeb5976d46ccb54
Lucas Savva <lucas@m1cr0man.com>
2020-08-01 10:03:57 +0100

    Remove acme gid

--- nixos/modules/misc/ids.nix
+++ nixos/modules/misc/ids.nix
@@ -647,7 +647,6 @@ in
       paperless = 315;
       #mailman = 316;  # removed 2019-08-30
       zigbee2mqtt = 317;
-      acme = 318;
 
       # When adding a gid, make sure it doesn't match an existing
       # uid. Users and groups with the same name should have equal
